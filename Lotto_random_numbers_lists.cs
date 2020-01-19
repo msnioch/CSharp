799ec3bc63db351fa0f1922c85a310e7a3624669
Mateusz Snioch <mateusz.snioch@gmail.com>
2020-01-19 17:46:05 +0100

    Lotto_random_numbers_lists
    Add class Lotto_generator and comments.

--- Lotto_random_numbers_lists.cs
+++ Lotto_random_numbers_lists.cs
@@ -1,50 +1,78 @@
-﻿using System;
+﻿/*  Lotto random numbers lists v2. The program generates a number of suggestions lists.
+ *  One suggestion list has 6 random numbers form 1 to 49. The numbers can't repeat.*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace My_Lotto_random_numbers
+namespace Lotto_random_numbers_lists
 {
-    class Program
+    class Lotto_app
     {
-        static void Main(string[] args)
+        class Lotto_generator
         {
-            List<int> li = new List<int>();         //Collection List<T>
+            private List<int> list = new List<int>();                   //Create collection List<T>
 
-            Random random = new Random();
-
-            for (int i = 0; i < 6; i++)
+            public void Print_list(int i, Random random)                //Print_list method of class Lotto_generator.
             {
-                int number = random.Next(1, 50);    //Random number from 1 to 49.
-
-                int a = 0;
-
-                while (a < li.Count)                 //The random numbers can't repeat.
+                for (int j = 0; j < 6; j++)
                 {
-                    if (li[a] == number)             //If the numbers repeat.
-                    {
-                        number = random.Next(1, 50);
+                    int number = random.Next(1, 50);                    //Create int random number from 1 to 49.
 
-                        a = 0;
-                    }
-                    else
+                    int a = 0;                                          //Declerate the counter a and assign by 0.
+
+                    while (a < list.Count)                              //The random numbers can't repeat.
                     {
-                        a++;
+                        if (list[a] == number)                          //Test if the numbers in the list and current namber are the same.
+                        {
+                            number = random.Next(1, 50);                //If true generates new random number.
+
+                            a = 0;                                      //If true assign the counter a by 0.
+                        }
+                        else
+                        {
+                            a++;                                        //If the numbers aren't the same increment the counter a by 1.
+                        }
                     }
+
+                    list.Add(number);                                   //Add number to the list.
                 }
 
-                li.Add(number);                     //Add number to the list.
+                list.Sort();                                            //Sort the numbers in the list.
+
+                Console.WriteLine("\nLotto numbers proposition " + (i + 1));
+
+                for (int k = 0; k < list.Count; k++)                    //Write the numbers list.
+                {
+                    Console.Write(list[k] + " ");
+                }
+
+                Console.WriteLine();
             }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many lotto suggestions should I generate? I can generate at most 25 suggestions.\n");
 
-            li.Sort();                              //Sort the numbers in the list.
+            Console.Write("Please enter a integer between 1 and 25: ");
 
-            for (int j = 0; j < li.Count; j++)      //Show the list.
+            int how = Convert.ToInt32(Console.ReadLine());              //Convert user input to int.
+
+            Lotto_generator[] suggestion = new Lotto_generator[how];    //Numbers sugesstion array of class Lotto_generator.
+
+            Random random = new Random();                               //Create random numbers of class Random.
+
+            for (int i = 0; i < how; i++)
             {
-                Console.Write(li[j] + " ");
+                suggestion[i] = new Lotto_generator();                  //Create new suggestion object of class Lotto_generator.
+                suggestion[i].Print_list(i, random);                    //Call Print_list method of object suggestion[i].
             }
 
-            Console.ReadKey();                      //To hold the programm.
+            Console.Write("\nGood Luck!");
+
+            Console.ReadKey();                                          //Hold console at end.
         }
     }
 }

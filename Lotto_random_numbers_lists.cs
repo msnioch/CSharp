3d578e9945ead185eff1f9328250deffcbf83b9e
Mateusz Snioch <mateusz.snioch@gmail.com>
2020-01-26 21:12:03 +0100

    Update Lotto_random_numbers_lists.cs
    Change variable names.

--- Lotto_random_numbers_lists.cs
+++ Lotto_random_numbers_lists.cs
@@ -15,34 +15,34 @@ namespace Lotto_random_numbers_lists
         {
             private List<int> list = new List<int>();                   //Create collection List<T>
 
-            public void Print_list(int i, Random random)                //Print_list method of class Lotto_generator.
+            public void Print_list(int number, Random random)           //Print_list method of class Lotto_generator.
             {
-                for (int j = 0; j < 6; j++)
+                for (int number_counter = 0; number_counter < 6; number_counter++)
                 {
-                    int number = random.Next(1, 50);                    //Create int random number from 1 to 49.
+                    int current_random = random.Next(1, 50);            //Create int random number from 1 to 49.
 
-                    int a = 0;                                          //Declerate the counter a and assign by 0.
+                    int test_counter = 0;                               //Declerate the counter and assign by 0.
 
-                    while (a < list.Count)                              //The random numbers can't repeat.
+                    while (test_counter < list.Count)                   //The random numbers can't repeat.
                     {
-                        if (list[a] == number)                          //Test if the numbers in the list and current namber are the same.
+                        if (list[test_counter] == current_random)       //Test if the numbers in the list and current namber are the same.
                         {
-                            number = random.Next(1, 50);                //If true generates new random number.
+                            current_random = random.Next(1, 50);        //If true generates new random number.
 
-                            a = 0;                                      //If true assign the counter a by 0.
+                            test_counter = 0;                           //If true assign the counter by 0.
                         }
                         else
                         {
-                            a++;                                        //If the numbers aren't the same increment the counter a by 1.
+                            test_counter++;                             //If the numbers aren't the same increment the counter by 1.
                         }
                     }
 
-                    list.Add(number);                                   //Add number to the list.
+                    list.Add(current_random);                           //Add number to the list.
                 }
 
                 list.Sort();                                            //Sort the numbers in the list.
 
-                Console.WriteLine("\nLotto numbers suggestions " + (i + 1));
+                Console.WriteLine("\nLotto numbers suggestion " + (number + 1));
 
                 for (int k = 0; k < list.Count; k++)                    //Write the numbers list.
                 {
@@ -75,10 +75,10 @@ namespace Lotto_random_numbers_lists
 
                 Random random = new Random();                               //Create random numbers of class Random.
 
-                for (int i = 0; i < how; i++)
+                for (int number = 0; number < how; number++)
                 {
-                    suggestion[i] = new Lotto_generator();                  //Create new suggestion object of class Lotto_generator.
-                    suggestion[i].Print_list(i, random);                    //Call Print_list method of object suggestion.
+                    suggestion[number] = new Lotto_generator();         //Create new suggestion object of class Lotto_generator.
+                    suggestion[number].Print_list(number, random);  //Call Print_list method of object suggestion.
                 }
 
                 Console.WriteLine("\nGood Luck!");

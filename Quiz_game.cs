b976327c3fb49caa1267a385c7a3106a9abd70cf
Mateusz Snioch <mateusz.snioch@gmail.com>
2020-02-04 21:55:26 +0100

    Update Quiz_game.cs
    Comments changed.

--- Quiz_game.cs
+++ Quiz_game.cs
@@ -1,4 +1,4 @@
-﻿/*Quiz game v 2 - it's a game that read the questions from a text file. The application'll try to open the text file in application folder. 
+﻿/*Quiz game v4 - it's a game that read the questions from a text file. The application'll try to open the text file in application folder. 
  * If opening the text file fails, you will be asked for the file path.*/
 
 using System;
@@ -11,44 +11,44 @@ namespace Quiz_app
 {
     class Quiz_game
     {
-        private static int points_Sum = 0;                                                           //Declarate a global integer of sum points.
+        private static int points_Sum = 0;                                                          //Declarate global integer of sum points.
 
-        private static string file_Patch = "Quiz.txt";                                                            //Declarate a global string of question file patch.
+        private static string file_Patch = "Quiz.txt";                                              //Declarate global string of question file patch.
 
         class Quiz
         {
-            private int number;                                                                      //Declarate a integer of question number.
+            private int number;                                                                     //Declarate integer of question number.
 
-            private string content;                                                                  //Declarate a string of content.
+            private string content;                                                                 //Declarate string of content.
 
-            private string answer_A, answer_B, answer_C, answer_D;                                   //Declarate a string of answer from a to d.
+            private string answer_A, answer_B, answer_C, answer_D;                                  //Declarate string of answer from a to d.
 
-            private string user_Answer, correct_Answer;                                              //Declarate a string of user and correct answer.
+            private string user_Answer, correct_Answer;                                             //Declarate string of user and correct answer.
 
             public Quiz (int num)                                                                   //Constructor.
             {
-                number = num;                                                                       //Assign a question number.
+                number = num;                                                                       //Assign variable question number.
 
-                Load();                                                                             //Calls a method Load().
+                Load();
             }
 
-            private void Load()                                                                     //Method Load().
+            private void Load()
             {
                 string [] lines = File.ReadAllLines (file_Patch);                                   //Assign file lines to string array.
 
-                int first_Line = (number - 1) * 6;                                                  //Compute the first number of question.
+                int first_Line = (number - 1) * 6;                                                  //Compute the first line number for current question.
 
-                content = lines [first_Line];                                                       //Assign line 1 to variable.
+                content = lines [first_Line];                                                       //Assign temporary_Line 0, 6, 12 ... to variable content.
 
-                answer_A = lines [first_Line + 1];                                                  //Assign line 2 to variable.
+                answer_A = lines [first_Line + 1];                                                  //Assign temporary_Line 1, 7, 13 ... to variable answer_A.
 
-                answer_B = lines [first_Line + 2];                                                  //Assign line 3 to variable.
+                answer_B = lines [first_Line + 2];                                                  //Assign temporary_Line 2, 8, 14 ... to variable answer_B.
 
-                answer_C = lines [first_Line + 3];                                                  //Assign line 4 to variable.
+                answer_C = lines [first_Line + 3];                                                  //Assign temporary_Line 3, 9, 15 ... to variable answer_C.
 
-                answer_D = lines [first_Line + 4];                                                  //Assign line 5 to variable.
+                answer_D = lines [first_Line + 4];                                                  //Assign temporary_Line 4, 10, 16 ... to variable answer_D.
 
-                correct_Answer = lines [first_Line + 5];                                            //Assign line 6 to variable.
+                correct_Answer = lines [first_Line + 5];                                            //Assign temporary_Line 5, 11, 17 ... to variable correct_Answer.
 
                 Show();                                                                             //Calls a method Show().
             }
@@ -65,7 +65,7 @@ namespace Quiz_app
 
                 Console.WriteLine("d) " + answer_D);                                                //Show the answer d.
 
-                Console.Write("\nWrite answer a, b, c or d: ");                                     //Wait for user answer.
+                Console.Write("\nWrite answer a, b, c or d: ");                                     //Show the user answers.
 
                 CheckAnswer();
             }
@@ -82,7 +82,7 @@ namespace Quiz_app
 
                     Console.WriteLine("\nGood! You get a point!");
 
-                    points_Sum += 1;                                                                //If user answer is true then add 1 point.
+                    points_Sum++;                                                                   //If user answer is true then add 1 point.
 
                     Thread.Sleep(2000);                                                             //Stop the console for 2 seconds.
                 }
@@ -95,22 +95,22 @@ namespace Quiz_app
 
                     Console.WriteLine("The correct answer is {0}!", correct_Answer);                //If user answer is false then show the correct answer.
 
-                    Thread.Sleep(6000);                                                             //Stop the console for 4 seconds.
+                    Thread.Sleep(6000);                                                             //Stop the console for 6 seconds.
                 }
             }
         }
 
-        static bool File_Exists(ref string patch)                                                   //Test if the quesion file exists.
+        static bool File_Exists (ref string patch)                                                  //Method test if the file exists with input of file patch (by reference).
         {
-            if (! File.Exists (patch))                                                              //Negation.
+            if (! File.Exists (patch))                                                              //Negation if the patch is correct.
             {
                 Console.Write("\nThe question file doesn't exists!\nPlease enter the quesion file patch or write END to exit application: ");
 
-                patch = Console.ReadLine();
+                patch = Console.ReadLine();                                                         //Change file patch or exit application with command END.
 
                 if (patch == "END")
                 {
-                    Environment.Exit(0);
+                    Environment.Exit(0);                                                            //Exit application.
                 }
 
                 return false;                                                                       //If the quesion file not exists return false.
@@ -128,15 +128,15 @@ namespace Quiz_app
 
             Console.Write("Press ENTER to start the Quiz. Good Luck!");
             
-            Console.ReadLine();
+            Console.ReadLine();                                                                     //Stop console.
 
-            while (! File_Exists (ref file_Patch));                                                 //Condition to stop the loop
+            while (! File_Exists (ref file_Patch));                                                 //If the file not exsist, the condition is true. Send file_Patch by reference.
 
-            int how_Many_Questions = File.ReadLines(file_Patch).Count() / 6;                        //Compute number of questions in file.
+            int how_Many_Questions = File.ReadLines(file_Patch).Count() / 6;                        //Compute number of questions in file. One question has 6 lines. Count() function return number of lines in file.
 
             Quiz [] Question = new Quiz [how_Many_Questions];                                       //Declarate array of class Question.
 
-            for (int question_Number = 0; question_Number < how_Many_Questions; question_Number++)  //For loop controls the question number.
+            for (int question_Number = 0; question_Number < how_Many_Questions; question_Number++)  //Loop controls question number.
             {
                 Console.Clear();                                                                    //The method clear console.
 
@@ -162,7 +162,7 @@ namespace Quiz_app
 
             Console.WriteLine("\n*************** End Quiz ***************");
 
-            Console.ReadLine();
+            Console.ReadLine();                                                                 //Stop console.
         }
     }
 }
